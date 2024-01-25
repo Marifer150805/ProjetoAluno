96f474783d2d0a99a6d97153cfc67477a3f124ad
Maria Fernanda <132359722+Marifer150805@users.noreply.github.com>
2024-01-25 16:42:19 -0300

    Add files via upload

---
+++ projetoControleAluno/src/main/java/com/projetojpa/controller/AlunoController.java
@@ -0,0 +1,46 @@
+package com.projetojpa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetojpa.entities.Aluno;
+import com.projetojpa.service.AlunoService;
+
+@RestController
+
+@RequestMapping("/alunos")
+
+public class AlunoController {
+
+	private final AlunoService alunoService;
+
+	@Autowired
+	public AlunoController(AlunoService alunoService) {
+		this.alunoService = alunoService;
+	}
+	@PostMapping
+	public Aluno createAluno(@RequestBody Aluno aluno) {
+		return alunoService.saveAluno(aluno);
+	}
+	@GetMapping("/{id}")
+	public Aluno getAluno(@PathVariable Long id) {
+		return alunoService.getAlunoById(id);
+	}
+	@GetMapping 
+	public List<Aluno> getAllAlunos(){
+		return alunoService.getAllAlunos();
+	}
+	@DeleteMapping("/{id}")
+	public void deleteAluno(@PathVariable Long id) {
+		alunoService.deleteAluno(id);
+	}
+
+}
---
+++ projetoControleAluno/src/main/java/com/projetojpa/service/AlunoService.java
@@ -0,0 +1,34 @@
+package com.projetojpa.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.projetojpa.entities.Aluno;
+import com.projetojpa.repository.AlunoRepository;
+
+@Service
+
+public class AlunoService {
+
+	private final AlunoRepository alunoRepository;
+
+	@Autowired
+	public AlunoService(AlunoRepository alunoRepository) {
+		this.alunoRepository = alunoRepository;
+	}
+	public Aluno saveAluno (Aluno aluno) {
+		return alunoRepository.save(aluno);
+	}
+	public Aluno getAlunoById(Long id) {
+		return alunoRepository.findById(id).orElse(null); 
+	}
+	public List<Aluno> getAllAlunos(){
+		return alunoRepository.findAll();
+	}
+	public void deleteAluno(Long id) {
+		alunoRepository.deleteById(id);
+	}
+}
+
+
+
